5467a1e66f2917eef8ae3fc06ede28b6c396181c
Yugesh <yugeshtg@gmail.com>
2025-07-29 17:49:42 +0530

    donate now

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,20 @@ const emergencyRequestBtn = document.getElementById("emergencyRequestBtn");
 const closeEmergencyFormBtn = document.getElementById("closeEmergencyFormBtn");
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
+const donateFormModal = document.getElementById("donateFormModal");
+const donateFormOverlay = document.getElementById("donateFormOverlay");
+const donateNowBtn = document.getElementById("donateNowBtn");
+const closeDonateFormBtn = document.getElementById("closeDonateFormBtn");
 
 emergencyRequestBtn.addEventListener("click", () => {
   emergencyFormModal.style.display = "block";
   emergencyFormOverlay.style.display = "block";
+});
+
+donateNowBtn.addEventListener("click", () => {
+  donateFormModal.style.display = "block";
+  donateFormOverlay.style.display = "block";
+  // donateFormModal.classList.add("slide-up");
 });
 
 function closeForm() {
@@ -15,7 +25,16 @@ function closeForm() {
   emergencyFormOverlay.style.display = "none";
 }
 
+function closeDonateForm() {
+  donateFormModal.style.display = "none";
+  donateFormOverlay.style.display = "none";
+  // donateFormModal.classList.remove("slide-up");
+}
+
 closeEmergencyFormBtn.addEventListener("click", closeForm);
+
+closeDonateFormBtn.addEventListener("click", closeDonateForm);
+
 
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show");
