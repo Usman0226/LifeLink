3ee4237c2bc63ad52735a4c2971d9a9533669786
Yugesh <yugeshtg@gmail.com>
2025-08-10 18:28:04 +0530

    SVG updates

--- Routes/getRoutes.js
+++ Routes/getRoutes.js
@@ -14,7 +14,7 @@ getRouter.get("/SignUp", (req, res) => {
   res.render(path.join(rootDir, "views","pages", "SignUp.ejs")); 
 });
 
-getRouter.get("/DashBoard",auth, (req, res) => {
+getRouter.get("/DashBoard", (req, res) => {
   res.render(path.join(rootDir,"views","pages", "DashBoard.ejs"),{
     jsfile : ['emergencyForm']
   });
